d3216ad9a03edf48b45988b1146b67f678f1d25f
ivan-clase <ivanfernandez830@gmail.com>
2024-11-21 12:49:27 +0100

    phonebook

--- 1ª evaluacion/Client/src/client/Contacts.java
+++ 1ª evaluacion/Client/src/client/Contacts.java
@@ -12,16 +12,58 @@ import java.util.Scanner;
  * @author ivan
  */
 public class Contacts {
-    HashMap<Integer, Client> clients = new HashMap<>();
+    private HashMap<Integer, Client> phonebook = new HashMap<>();
+
+    /**
+     * devolve os valores do array phonebook
+     * @return 
+     */
+    public HashMap<Integer, Client> getPhonebook() {
+        return phonebook;
+    }
+
+    /**
+     * modifica o valor do array phonebook
+     * @param phonebook 
+     */
+    public void setPhonebook(HashMap<Integer, Client> phonebook) {
+        this.phonebook = phonebook;
+    }
+
+   
+    
+    
     
     private void insert(int tel, Client cli) {
+        
+        
+        // añade un client ao array coa sua key e o seu obxeto asociado
+        phonebook.put(tel, cli);
+    }
+    
+    private void remove(int tel) {
+        phonebook.remove(tel);        
+    }
+    
+    private Client findByNumber(int tel) {
+        return phonebook.get(tel);
+    }
+    
+    private int numContacts() {
+     return 3;
+    }
+    
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
+        Contacts contacts = new Contacts();
         
-        // pedimos por teclado o teléfono, nif, name e surname e gardamos cada un na sua 
-        // variable correspondente
+        // pedimos por teclado o teléfono, nif, name e surname e gardamos
+        // cada un na sua variable correspondente
+        
         System.out.println("introduce o teléfono: ");
-        int tel = scan.nextLine();
+        int tel = scan.nextInt();
+        scan.nextLine();
         
         System.out.println("introduce o nif: ");
         String nif = scan.nextLine();
@@ -36,25 +78,55 @@ public class Contacts {
         System.out.println();
         
         // creamos o obxeto client
-        Client client = new Client(nif, name, surname);
+        Client client1 = new Client(nif, name, surname);
         
-        // añade un client ao array coa sua key e o seu obxeto asociado
-        clients.put(tel, client);
-    }
-    
-    private void remove(int tel) {
+        // insertamos un cliente no array
+        phonebook.insert(tel, client1);
         
-    }
-    
-    private Client findByNumber(int tel) {
+        // añadimos outro cliente 
+        System.out.println("introduce o teléfono: ");
+        tel = scan.nextInt();
+        scan.nextLine();
         
-    }
-    
-    private int numContacts() {
+        System.out.println("introduce o nif: ");
+        nif = scan.nextLine();
         
-    }
-    
-    public static void main(String[] args) {
+        System.out.println("introduce o nome: ");
+        name = scan.nextLine();
+        
+        System.out.println("introduce o apelido: ");
+        surname = scan.nextLine();
+        
+        // salto de liña
+        System.out.println();
+        
+        // creamos outro cliente
+        Client client2 = new Client(nif, name, surname);
+        
+        // insertamos un cliente no array
+        contacts.insert(tel, client2);
+        
+        
+        
+        // mostramos cantos contactos ten o array
+        contacts.numContacts();
+        
+        // buscamos a un cliente polo seu número de teléfono e mostrámolo
+        // por pantalla
+        System.out.println("indica o número de teléfono do cliente que queres buscar");
+        tel = scan.nextInt();        
+        System.out.println("o cliente co número de teléfono: "+ tel + " é: " + phonebook.findByNumber(tel));
+        
+        System.out.println("introduce o teléfono do cliente que queiras eliminar: ");
+        tel = scan.nextInt();
+        contacts.remove(tel); 
+        
+        // mostramos os clientes que quedan no array despois de borrar o anterior
+        contacts.numContacts();
+        
+        
+        
+        
         
     }
     
