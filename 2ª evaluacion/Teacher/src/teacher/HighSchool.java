972796af5b84a283fe31f158a24134a320114789
ivan-clase <ivanfernandez830@gmail.com>
2025-01-14 12:57:55 +0100

    subclase school

---
+++ 2ª evaluacion/Teacher/src/teacher/HighSchool.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package teacher;
+
+/**
+ *
+ * @author ivan.fernandezguinde
+ */
+public class HighSchool extends Teacher{
+    
+    String name;
+    
+}
