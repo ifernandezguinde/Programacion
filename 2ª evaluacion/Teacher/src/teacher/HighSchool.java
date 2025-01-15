3b754456ced4a70fd4943ecb04fc3b2f41ce133d
Ivan Fernandez Guinde <ivanfernandez830@gmail.com>
2025-01-15 12:23:40 +0100

    metodos highschool

--- 2ª evaluacion/Teacher/src/teacher/HighSchool.java
+++ 2ª evaluacion/Teacher/src/teacher/HighSchool.java
@@ -4,12 +4,105 @@
  */
 package teacher;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ivan.fernandezguinde
  */
-public class HighSchool extends Teacher{
+public class HighSchool {
     
-    String name;
+    private String name;
+    private ArrayList<Teacher> teachers;
+
+    /**
+     * da un valor as variables
+     * @param name
+     * @param teachers 
+     */
+    public HighSchool(String name, ArrayList<Teacher> teachers) {
+        this.name = name;
+        this.teachers = teachers;
+    }
+
+    /**
+     * devolve o valor de name
+     * @return 
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * modifica o valor de name
+     * @param name 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * devolve o valor do ArrayList
+     * @return 
+     */
+    public ArrayList<Teacher> getTeachers() {
+        return teachers;
+    }
+
+        /**
+         * modifica o valor do ArrayList
+         * @param teachers 
+         */
+    public void setTeachers(ArrayList<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+    
+    
+    
+    
+    
+    
+    
+    public Teacher TeacherMostPaid() {
+        
+        Teacher mostPaid = teachers.get(0);
+        for (Teacher teacher : teachers) {
+            if (teacher.getSalary() > mostPaid.getSalary()) {
+                mostPaid = teacher;
+            }
+        }
+        return mostPaid;
+    }
+    
+    public Teacher TeacherLeastPaid() {
+        
+        Teacher leastPaid = teachers.get(0);
+        for (Teacher teacher : teachers) {
+            if (teacher.getSalary() < leastPaid.getSalary()) {
+                leastPaid = teacher;
+            }
+        }
+        return leastPaid;
+    }
+    
+    public double SalaryCosts() {
+        
+        double total = 0;
+        for (Teacher teacher : teachers) {
+            total += teacher.getSalary();
+        }
+        return total;
+    }
+    
+    public double SalaryAverage() {
+        
+        return this.SalaryCosts() / teachers.size();
+    }
+    
+    
+    public static void main(String[] args) {
+        HighSchool highSchool = new HighSchool("Instituto Montealto");
+    }
+    
     
 }
