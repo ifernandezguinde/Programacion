fec9fa2f5ac3150a58f5826233c9075f393e856f
ivan-clase <ivanfernandez830@gmail.com>
2025-02-18 12:50:00 +0100

    5.4.3

---
+++ 2ª evaluacion/5.4.2/src/pkg5/pkg4/pkg2/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg5.pkg4.pkg2;
+
+/**
+ *
+ * @author ivan.fernandezguinde
+ */
+public class Main {
+
+}
